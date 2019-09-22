da31201cdc952b2a7ecb06281d83c2709bb626d8
Bartosz Gołek <bartosz.golek@gmail.com>
2019-09-22 23:09:56 +0200

    versioning using tags

--- pack.sh
+++ pack.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
+function join_by { local IFS="$1"; shift; echo "$*"; }
+
+TAG=$(git describe --tags)
+IFS='-'
+read -ra VERSION_A <<< "$TAG"
+IFS=' '
+
 for csproj in `find . -name '*.csproj'`
 do
-    dotnet pack -c Release ${csproj} -o $(pwd)
+    if [[ ${#VERSION_A[@]} -eq 1 ]]; then
+        dotnet pack -c Release ${csproj} /p:Version=${TAG} -o $(pwd)
+    else
+        SUFFIX_A="${VERSION_A[@]:1}"
+        VERSION_SUFFIX=$(join_by - ${SUFFIX_A})
+        dotnet pack -c Release ${csproj} /p:Version=${TAG} --version-suffix ${VERSION_SUFFIX} -o $(pwd)
+    fi
 done
