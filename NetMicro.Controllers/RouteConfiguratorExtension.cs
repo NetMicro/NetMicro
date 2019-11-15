1c7949674b33963c8932be8bc1d398bf77e49a4e
Bartosz Gołek <bartosz.golek@pearson.com>
2019-11-15 09:37:43 +0100

    Fixed controller extension

--- NetMicro.Controllers/RouteConfiguratorExtension.cs
+++ NetMicro.Controllers/RouteConfiguratorExtension.cs
@@ -4,74 +4,74 @@ namespace NetMicro.Controllers
 {
     public static class RouteConfiguratorExtension
     {
-        public static IRoute Get<TRoute, TRequestController>(this IRouteConfigurator routeConfigurator,
+        public static IRoute Get<TRequestController>(this IRouteConfigurator routeConfigurator,
             string name, string path,
             Controller<TRequestController> controller
-        ) where TRoute : IRoute where TRequestController : IRequestController
+        ) where TRequestController : IRequestController
         {
             return routeConfigurator.Add("GET", name, path, controller.Execute);
         }
 
-        public static IRoute Head<TRoute, TRequestController>(this IRouteConfigurator routeConfigurator,
+        public static IRoute Head<TRequestController>(this IRouteConfigurator routeConfigurator,
             string name, string path,
             Controller<TRequestController> controller
-        ) where TRoute : IRoute where TRequestController : IRequestController
+        ) where TRequestController : IRequestController
         {
             return routeConfigurator.Add("HEAD", name, path, controller.Execute);
         }
 
-        public static IRoute Post<TRoute, TRequestController>(this IRouteConfigurator routeConfigurator,
+        public static IRoute Post<TRequestController>(this IRouteConfigurator routeConfigurator,
             string name, string path,
             Controller<TRequestController> controller
-        ) where TRoute : IRoute where TRequestController : IRequestController
+        ) where TRequestController : IRequestController
         {
             return routeConfigurator.Add("POST", name, path, controller.Execute);
         }
 
-        public static IRoute Put<TRoute, TRequestController>(this IRouteConfigurator routeConfigurator,
+        public static IRoute Put<TRequestController>(this IRouteConfigurator routeConfigurator,
             string name, string path,
             Controller<TRequestController> controller
-        ) where TRoute : IRoute where TRequestController : IRequestController
+        ) where TRequestController : IRequestController
         {
             return routeConfigurator.Add("PUT", name, path, controller.Execute);
         }
 
-        public static IRoute Delete<TRoute, TRequestController>(this IRouteConfigurator routeConfigurator,
+        public static IRoute Delete<TRequestController>(this IRouteConfigurator routeConfigurator,
             string name, string path,
             Controller<TRequestController> controller
-        ) where TRoute : IRoute where TRequestController : IRequestController
+        ) where TRequestController : IRequestController
         {
             return routeConfigurator.Add("DELETE", name, path, controller.Execute);
         }
 
-        public static IRoute Connect<TRoute, TRequestController>(this IRouteConfigurator routeConfigurator,
+        public static IRoute Connect<TRequestController>(this IRouteConfigurator routeConfigurator,
             string name, string path,
             Controller<TRequestController> controller
-        ) where TRoute : IRoute where TRequestController : IRequestController
+        ) where TRequestController : IRequestController
         {
             return routeConfigurator.Add("CONNECT", name, path, controller.Execute);
         }
 
-        public static IRoute Options<TRoute, TRequestController>(this IRouteConfigurator routeConfigurator,
+        public static IRoute Options<TRequestController>(this IRouteConfigurator routeConfigurator,
             string name, string path,
             Controller<TRequestController> controller
-        ) where TRoute : IRoute where TRequestController : IRequestController
+        ) where TRequestController : IRequestController
         {
             return routeConfigurator.Add("OPTIONS", name, path, controller.Execute);
         }
 
-        public static IRoute Trace<TRoute, TRequestController>(this IRouteConfigurator routeConfigurator,
+        public static IRoute Trace<TRequestController>(this IRouteConfigurator routeConfigurator,
             string name, string path,
             Controller<TRequestController> controller
-        ) where TRoute : IRoute where TRequestController : IRequestController
+        ) where TRequestController : IRequestController
         {
             return routeConfigurator.Add("TRACE", name, path, controller.Execute);
         }
 
-        public static IRoute Patch<TRoute, TRequestController>(this IRouteConfigurator routeConfigurator,
+        public static IRoute Patch<TRequestController>(this IRouteConfigurator routeConfigurator,
             string name, string path,
             Controller<TRequestController> controller
-        ) where TRoute : IRoute where TRequestController : IRequestController
+        ) where TRequestController : IRequestController
         {
             return routeConfigurator.Add("PATCH", name, path, controller.Execute);
         }
