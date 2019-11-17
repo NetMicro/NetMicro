04626260a457729bdb9529b4220953ec6d65b7cd
Bartosz Gołek <bartosz.golek@gmail.com>
2019-11-17 23:55:10 +0100

    Fixed private key NFlags option name

--- NetMicro.Auth.Jwt.NFlags/AuthOptions.cs
+++ NetMicro.Auth.Jwt.NFlags/AuthOptions.cs
@@ -3,7 +3,7 @@ namespace NetMicro.Auth.Jwt.NFlags
     public static class AuthOptions
     {
         public const string AuthPublicKey = "auth_public_key";
-        public const string AuthPrivateKey = "auth_public_key";
+        public const string AuthPrivateKey = "auth_private_key";
         public const string AuthSecret = "auth_secret";
     }
 }
