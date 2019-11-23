905c7f09524eed5c5e065014792d8d23d576a07f
Bartosz Gołek <bartosz.golek@pearson.com>
2019-11-23 19:56:55 +0100

    Fixed matching for CORS OPTIONS call.

--- NetMicro.Routing.Extensions.Cors/HandleCorsOptionsMiddleware.cs
+++ NetMicro.Routing.Extensions.Cors/HandleCorsOptionsMiddleware.cs
@@ -8,7 +8,13 @@ namespace NetMicro.Routing.Extensions.Cors
         public static void EnableCorsOptionsRequestHandling(this IRouteConfigurator router,
             Action<CorsOptions> setOptions = null)
         {
-            router.Options("cors_preflight", "/*",context =>
+            router.Options("cors_preflight", "*",context =>
+            {
+                return Task.Run(() =>
+                    context.Response.SetCorsHeaders(CorsOptions.GetCorsOptions(setOptions)));
+            });
+
+            router.Options("cors_preflight", "/",context =>
             {
                 return Task.Run(() =>
                     context.Response.SetCorsHeaders(CorsOptions.GetCorsOptions(setOptions)));
