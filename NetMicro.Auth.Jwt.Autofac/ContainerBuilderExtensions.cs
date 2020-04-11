6ba9929ac2c60fc94304b73b196c6fdece2a8bb0
Bartosz Gołek <bartosz.golek@pearson.com>
2020-04-11 22:17:19 +0200

    easy token registration

--- NetMicro.Auth.Jwt.Autofac/ContainerBuilderExtensions.cs
+++ NetMicro.Auth.Jwt.Autofac/ContainerBuilderExtensions.cs
@@ -1,12 +1,67 @@
+using System;
 using Autofac;
+using Org.BouncyCastle.OpenSsl;
 
 namespace NetMicro.Auth.Jwt.Autofac
 {
     public static class ContainerBuilderExtensions
     {
-        public static void UseJwtAuth(this ContainerBuilder builder)
+        public const string TokenGenerator = "TokenHenerator";
+        public const string TokenDecoder = "TokenDecoder";
+
+        private const string TokenPrivateKeyProvider = "TokenPrivateKeyProvider";
+        private const string TokenPublicKeyProvider = "TokenPublicKeyProvider";
+        private const string TokenConfigPasswordFinder = "TokenConfigPasswordFinder";
+
+        public static ContainerBuilder UseJwtAuth(this ContainerBuilder builder)
         {
             builder.RegisterModule<RsaAuthModule>();
+            return builder;
+        }
+
+        public static ContainerBuilder RegisterToken<TPasswordFinder>(
+            this ContainerBuilder builder,
+            string tokenName,
+            Func<IComponentContext, string> publicKeyProvider,
+            Func<IComponentContext, string> privateKeyProvider
+            )
+        {
+            builder
+                .Register(context => new FileKeyProvider(privateKeyProvider(context)))
+                .As<IKeyProvider>()
+                .Named<IKeyProvider>(tokenName + TokenPrivateKeyProvider)
+                .SingleInstance();
+
+            builder
+                .Register(c => new RsaTokenGenerator<JwtToken>(
+                    c.ResolveNamed<IPasswordFinder>(tokenName + TokenConfigPasswordFinder),
+                    c.ResolveNamed<IKeyProvider>(tokenName + TokenPrivateKeyProvider)
+                ))
+                .As<ITokenGenerator<JwtToken>>()
+                .Named<ITokenGenerator<JwtToken>>(tokenName + TokenGenerator)
+                .SingleInstance();
+
+            builder
+                .Register(context => new FileKeyProvider(publicKeyProvider(context)))
+                .As<IKeyProvider>()
+                .Named<IKeyProvider>(tokenName + TokenPublicKeyProvider)
+                .SingleInstance();
+
+            builder
+                .Register(c => new RsaTokenDecoder<JwtToken>(
+                    c.ResolveNamed<IPasswordFinder>(tokenName + TokenConfigPasswordFinder),
+                    c.ResolveNamed<IKeyProvider>(tokenName + TokenPublicKeyProvider)
+                ))
+                .As<ITokenDecoder<JwtToken>>()
+                .Named<ITokenDecoder<JwtToken>>(tokenName + TokenDecoder)
+                .SingleInstance();
+
+            builder
+                .RegisterType<TPasswordFinder>()
+                .As<IPasswordFinder>().SingleInstance()
+                .Named<IPasswordFinder>(tokenName + TokenConfigPasswordFinder);
+
+            return builder;
         }
     }
 }
--- NetMicro.Auth.Jwt.Autofac/RsaAuthModule.cs
+++ NetMicro.Auth.Jwt.Autofac/RsaAuthModule.cs
@@ -1,106 +1,31 @@
 using Autofac;
-using Org.BouncyCastle.OpenSsl;
 
 namespace NetMicro.Auth.Jwt.Autofac
 {
     public class RsaAuthModule : Module
     {
-        public const string AccessTokenGenerator = "AccessTokenGenerator";
-        public const string AccessTokenDecoder = "AccessTokenDecoder";
+        public const string Access = "Access";
+        public const string Refresh = "Refresh";
 
-        public const string RefreshTokenGenerator = "RefreshTokenGenerator";
-        public const string RefreshTokenDecoder = "RefreshTokenDecoder";
+        public const string AccessTokenGenerator = Access + ContainerBuilderExtensions.TokenGenerator;
+        public const string AccessTokenDecoder = Access + ContainerBuilderExtensions.TokenDecoder;
 
-        private const string AccessTokenPrivateKeyProvider = "AccessTokenPrivateKeyProvider";
-        private const string AccessTokenPublicKeyProvider = "AccessTokenPublicKeyProvider";
-        private const string AccessTokenSecretProvider = "AccessTokenSecretProvider";
-        private const string AccessTokenConfigPasswordFinder = "AccessTokenConfigPasswordFinder";
-
-        private const string RefreshTokenPrivateKeyProvider = "RefreshTokenPrivateKeyProvider";
-        private const string RefreshTokenPublicKeyProvider = "RefreshTokenPublicKeyProvider";
-        private const string RefreshTokenSecretProvider = "RefreshTokenSecretProvider";
-        private const string RefreshTokenConfigPasswordFinder = "AccessTokenConfigPasswordFinder";
+        public const string RefreshTokenGenerator = Refresh + ContainerBuilderExtensions.TokenGenerator;
+        public const string RefreshTokenDecoder = Refresh + ContainerBuilderExtensions.TokenDecoder;
 
         protected override void Load(ContainerBuilder builder)
         {
-            RegisterAccessToken(builder);
-            RegisterRefreshToken(builder);
-        }
+            builder.RegisterToken<AccessTokenConfigPasswordFinder>(
+                Access,
+                context => context.Resolve<IAuthConfiguration>().AccessTokenPublicKey,
+                context => context.Resolve<IAuthConfiguration>().AccessTokenPrivateKey
+            );
 
-        private static void RegisterAccessToken(ContainerBuilder builder)
-        {
-            builder
-                .Register(context => new FileKeyProvider(context.Resolve<IAuthConfiguration>().AccessTokenPrivateKey))
-                .As<IKeyProvider>()
-                .Named<IKeyProvider>(AccessTokenPrivateKeyProvider)
-                .SingleInstance();
-
-            builder
-                .Register(c => new RsaTokenGenerator<JwtToken>(
-                    c.ResolveNamed<IPasswordFinder>(AccessTokenConfigPasswordFinder),
-                    c.ResolveNamed<IKeyProvider>(AccessTokenPrivateKeyProvider)
-                ))
-                .As<ITokenGenerator<JwtToken>>()
-                .Named<ITokenGenerator<JwtToken>>(AccessTokenGenerator)
-                .SingleInstance();
-
-            builder
-                .Register(context => new FileKeyProvider(context.Resolve<IAuthConfiguration>().AccessTokenPublicKey))
-                .As<IKeyProvider>()
-                .Named<IKeyProvider>(AccessTokenPublicKeyProvider)
-                .SingleInstance();
-
-            builder
-                .Register(c => new RsaTokenDecoder<JwtToken>(
-                    c.ResolveNamed<IPasswordFinder>(AccessTokenConfigPasswordFinder),
-                    c.ResolveNamed<IKeyProvider>(AccessTokenPublicKeyProvider)
-                ))
-                .As<ITokenDecoder<JwtToken>>()
-                .Named<ITokenDecoder<JwtToken>>(AccessTokenDecoder)
-                .SingleInstance();
-
-            builder
-                .RegisterType<AccessTokenConfigPasswordFinder>()
-                .As<IPasswordFinder>().SingleInstance()
-                .Named<IPasswordFinder>(AccessTokenConfigPasswordFinder);
-        }
-
-        private static void RegisterRefreshToken(ContainerBuilder builder)
-        {
-            builder
-                .Register(context => new FileKeyProvider(context.Resolve<IAuthConfiguration>().RefreshTokenPrivateKey))
-                .As<IKeyProvider>()
-                .Named<IKeyProvider>(RefreshTokenPrivateKeyProvider)
-                .SingleInstance();
-
-            builder
-                .Register(c => new RsaTokenGenerator<JwtToken>(
-                    c.ResolveNamed<IPasswordFinder>(RefreshTokenConfigPasswordFinder),
-                    c.ResolveNamed<IKeyProvider>(RefreshTokenPrivateKeyProvider)
-                ))
-                .As<ITokenGenerator<JwtToken>>()
-                .Named<ITokenGenerator<JwtToken>>(RefreshTokenGenerator)
-                .SingleInstance();
-
-            builder
-                .Register(context => new FileKeyProvider(context.Resolve<IAuthConfiguration>().RefreshTokenPublicKey))
-                .As<IKeyProvider>()
-                .Named<IKeyProvider>(RefreshTokenPublicKeyProvider)
-                .SingleInstance();
-
-            builder
-                .Register(c => new RsaTokenDecoder<JwtToken>(
-                    c.ResolveNamed<IPasswordFinder>(RefreshTokenConfigPasswordFinder),
-                    c.ResolveNamed<IKeyProvider>(RefreshTokenPublicKeyProvider)
-                ))
-                .As<ITokenDecoder<JwtToken>>()
-                .Named<ITokenDecoder<JwtToken>>(RefreshTokenDecoder)
-                .SingleInstance();
-
-            builder
-                .RegisterType<RefreshTokenConfigPasswordFinder>()
-                .As<IPasswordFinder>().SingleInstance()
-                .Named<IPasswordFinder>(RefreshTokenConfigPasswordFinder);
+            builder.RegisterToken<RefreshTokenConfigPasswordFinder>(
+                Refresh,
+                context => context.Resolve<IAuthConfiguration>().RefreshTokenPublicKey,
+                context => context.Resolve<IAuthConfiguration>().RefreshTokenPrivateKey
+            );
         }
     }
 }
