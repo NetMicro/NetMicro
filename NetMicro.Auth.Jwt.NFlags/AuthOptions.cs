f79f1e130c65f09721c89a4d0352d3d109c8d8af
Bartosz Gołek <bartosz.golek@pearson.com>
2020-04-13 13:33:11 +0200

    Easier token registration in NFlags

--- NetMicro.Auth.Jwt.NFlags/AuthOptions.cs
+++ NetMicro.Auth.Jwt.NFlags/AuthOptions.cs
@@ -2,6 +2,13 @@ namespace NetMicro.Auth.Jwt.NFlags
 {
     public static class AuthOptions
     {
+        public const string AccessTokenName = "Access";
+        public const string RefreshTokenName = "Refresh";
+
+        public const string TokenPublicKeySuffix = "-token-public-key";
+        public const string TokenPrivateKeySuffix = "-token-private-key";
+        public const string TokenSecretSuffix = "-token-secret";
+
         public const string AccessTokenPublicKey = "access-token-public-key";
         public const string AccessTokenPrivateKey = "access-token-private-key";
         public const string AccessTokenSecret = "access-token-secret";
--- NetMicro.Auth.Jwt.NFlags/CommandConfiguratorExtensions.cs
+++ NetMicro.Auth.Jwt.NFlags/CommandConfiguratorExtensions.cs
@@ -9,64 +9,45 @@ namespace NetMicro.Auth.Jwt.NFlags
         public static CommandConfigurator RegisterAuth(this CommandConfigurator commandConfigurator)
         {
             return commandConfigurator
-                .RegisterRefreshToken()
-                .RegisterAccessTokenRead()
-                .RegisterAccessTokenWrite();
+                .RegisterToken(AuthOptions.AccessTokenName)
+                .RegisterToken(AuthOptions.RefreshTokenName);
+
         }
 
-        public static CommandConfigurator RegisterAccessTokenRead(this CommandConfigurator commandConfigurator)
+        public static CommandConfigurator RegisterToken(this CommandConfigurator commandConfigurator, string tokenName)
+        {
+            return commandConfigurator
+                .RegisterTokenRead(tokenName)
+                .RegisterTokenWrite(tokenName);
+        }
+
+        public static CommandConfigurator RegisterTokenWrite(this CommandConfigurator commandConfigurator, string tokenName)
         {
             return commandConfigurator
                 .RegisterOption<string>(b => b
-                    .Name(AuthOptions.AccessTokenPublicKey)
-                    .Description("Public key used to decode access token")
-                    .ConfigPath("Auth:AccessToken:PublicKey")
-                    .EnvironmentVariable("ACCESS_TOKEN_PUBLIC_KEY")
-                    .Group(AuthorizationGroup)
-                )
-                .RegisterOption<string>(b => b
-                    .Name(AuthOptions.AccessTokenSecret)
-                    .Description("Secret for access token keys")
-                    .ConfigPath("Auth:AccessToken:Secret")
-                    .EnvironmentVariable("ACCESS_TOKEN_SECRET")
+                    .Name(tokenName.ToLower() + AuthOptions.TokenPrivateKeySuffix)
+                    .Description($"Private key used to generate {tokenName.ToLower()} token")
+                    .ConfigPath($"Auth:{tokenName}Token:PrivateKey")
+                    .EnvironmentVariable($"{tokenName.ToUpper()}_TOKEN_PRIVATE_KEY")
                     .Group(AuthorizationGroup)
                 );
         }
 
-        private static CommandConfigurator RegisterRefreshToken(this CommandConfigurator commandConfigurator)
+        public static CommandConfigurator RegisterTokenRead(this CommandConfigurator commandConfigurator, string tokenName)
         {
             return commandConfigurator
                 .RegisterOption<string>(b => b
-                    .Name(AuthOptions.RefreshTokenPublicKey)
-                    .Description("Public key used to decode refresh token")
-                    .ConfigPath("Auth:RefreshToken:PublicKey")
-                    .EnvironmentVariable("REFRESH_TOKEN_PUBLIC_KEY")
+                    .Name(tokenName.ToLower() + AuthOptions.TokenPublicKeySuffix)
+                    .Description($"Public key used to decode {tokenName.ToLower()} token")
+                    .ConfigPath($"Auth:{tokenName}Token:PublicKey")
+                    .EnvironmentVariable($"{tokenName.ToUpper()}_TOKEN_PUBLIC_KEY")
                     .Group(AuthorizationGroup)
                 )
                 .RegisterOption<string>(b => b
-                    .Name(AuthOptions.RefreshTokenPrivateKey)
-                    .Description("Private key used to generate refresh token")
-                    .ConfigPath("Auth:RefreshToken:PrivateKey")
-                    .EnvironmentVariable("REFRESH_TOKEN_PRIVATE_KEY")
-                    .Group(AuthorizationGroup)
-                )
-                .RegisterOption<string>(b => b
-                    .Name(AuthOptions.RefreshTokenSecret)
-                    .Description("Secret for refresh token keys")
-                    .ConfigPath("Auth:RefreshToken:Secret")
-                    .EnvironmentVariable("REFRESH_TOKEN_SECRET")
-                    .Group(AuthorizationGroup)
-                );
-        }
-
-        private static CommandConfigurator RegisterAccessTokenWrite(this CommandConfigurator commandConfigurator)
-        {
-            return commandConfigurator
-                .RegisterOption<string>(b => b
-                    .Name(AuthOptions.AccessTokenPrivateKey)
-                    .Description("Private key used to generate access token")
-                    .ConfigPath("Auth:AccessToken:PrivateKey")
-                    .EnvironmentVariable("ACCESS_TOKEN_PRIVATE_KEY")
+                    .Name(tokenName.ToLower() + AuthOptions.TokenSecretSuffix)
+                    .Description($"Secret for {tokenName.ToLower()} token keys")
+                    .ConfigPath($"Auth:{tokenName}Token:Secret")
+                    .EnvironmentVariable($"{tokenName.ToUpper()}_TOKEN_SECRET")
                     .Group(AuthorizationGroup)
                 );
         }
